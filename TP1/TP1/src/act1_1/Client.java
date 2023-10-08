d55c9692194dbddd16af27fabb99ad82a92761f0
'Racem' <'racem.hachicha1@gamil.com'>
2023-10-08 20:37:12 +0100

    Initial commit

---
+++ TP1/TP1/src/act1_1/Client.java
@@ -0,0 +1,23 @@
+package act1_1;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+	public static void main(String arg[]) {
+		try{
+			// Attentative de connexion au serveur utilisant l'addresse 'localhost' et le port 1234.
+			Socket socket = new Socket("localhost", 1234);
+			System.out.println("Je suis connecté");
+			
+			// Liberer les resources
+			socket.close();
+		}catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
